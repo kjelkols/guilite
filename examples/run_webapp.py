1f858ead2c14fd13b43b6d5e9b575ba1e810ae22
Kjell Kolsaker <kjell.kolsaker@gmail.com>
2025-09-22 17:05:37 +0200

    Klar for versjon 0.1.0

---
+++ examples/run_webapp.py
@@ -0,0 +1,10 @@
+from reportgenerator.simulatorapp import SimulatorApp
+from examples.add_and_multiply_simulators import MySimulator1, MySimulator2
+
+if __name__ == "__main__":
+    simulators = {
+        "add": MySimulator1,
+        "mult": MySimulator2,
+    }
+    app = SimulatorApp(simulators=simulators)
+    app.run(debug=True)
